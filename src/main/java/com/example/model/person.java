54e74b8325a8a204707fbc6b1195fa32723da638
hafizhdi06 <hafizulhdi08@gmail.com>
2026-01-29 14:34:11 +0700

    Update person.java

---
+++ src/main/java/com/example/model/person.java
@@ -0,0 +1,34 @@
+package com.hotel.model;
+
+public abstract class Person {
+
+    protected String id;
+    protected String nama;
+    protected String noTelp;
+    protected String alamat;
+
+    public Person(String id, String nama, String noTelp, String alamat) {
+        this.id = id;
+        this.nama = nama;
+        this.noTelp = noTelp;
+        this.alamat = alamat;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNoTelp() {
+        return noTelp;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public abstract String getInfo();
+}
