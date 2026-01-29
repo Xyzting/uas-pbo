fa82a0a41a1a97c2ec5f776edfd886cb77c5333d
hafizhdi06 <hafizulhdi08@gmail.com>
2026-01-29 22:23:39 +0700

    Add Karyawan class extending Person

---
+++ src/main/java/com/example/model/karyawan.java
@@ -0,0 +1,24 @@
+package com.hotel.model;
+
+public class Karyawan extends Person {
+
+    private String shift;
+
+    public Karyawan(String id, String nama, String noTelp, String alamat, String shift) {
+        super(id, nama, noTelp, alamat);
+        this.shift = shift;
+    }
+
+    public String getShift() {
+        return shift;
+    }
+
+    public void setShift(String shift) {
+        this.shift = shift;
+    }
+
+    @Override
+    public String getInfo() {
+        return "Karyawan: " + nama + " | Shift: " + shift;
+    }
+}
