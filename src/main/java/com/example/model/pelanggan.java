1a0199cdcc54da6bd1f8aedf740f074de5182515
hafizhdi06 <hafizulhdi08@gmail.com>
2026-01-29 22:24:35 +0700

    Add Pelanggan class extending Person

---
+++ src/main/java/com/example/model/pelanggan.java
@@ -0,0 +1,20 @@
+package com.hotel.model;
+
+public class Pelanggan extends Person {
+
+    private String noKtp;
+
+    public Pelanggan(String id, String nama, String noTelp, String alamat, String noKtp) {
+        super(id, nama, noTelp, alamat);
+        this.noKtp = noKtp;
+    }
+
+    public String getNoKtp() {
+        return noKtp;
+    }
+
+    @Override
+    public String getInfo() {
+        return "Pelanggan: " + nama + " | KTP: " + noKtp;
+    }
+}
