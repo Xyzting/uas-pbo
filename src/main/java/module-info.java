1bbb8c4d26c7d7dd9433d29b4cfbe102221c97f8
Xyzting <reyhan.fathir1110@gmail.com>
2026-01-31 02:09:41 +0700

    feat; export new module to project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,18 @@
 module com.example {
     requires javafx.controls;
     requires javafx.fxml;
-
+    
+    // BCrypt untuk password hashing
+    requires jbcrypt;
+    
+    // Opens untuk JavaFX FXML
     opens com.example to javafx.fxml;
     opens com.example.controller to javafx.fxml;
+    opens com.example.model to javafx.base; 
 
+    // Exports
     exports com.example;
     exports com.example.controller;
+    exports com.example.model;
+    exports com.example.utils;
 }
